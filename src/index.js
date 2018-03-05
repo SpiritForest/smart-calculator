12b459eb7f1f329411e88eab200cf9567f18f751
Dzmitry Zakhar <31858665+SpiritForest@users.noreply.github.com>
2018-03-05 17:44:44 +0300

    2
    2

--- src/index.js
+++ src/index.js
@@ -1,26 +1,66 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
+  constructor(initValue) {
+    this.init = initValue;
+    this.operations = [];
   }
+  get result() {
+    let rpn = [this.init]; // reverse polish notation
+    let ops = [];
 
-  add(number) {
-    // your implementation
+    this.operations.forEach(op => { // сортировочная станция
+      while (ops.length && (ops[ops.length - 1]).priority < op.priority) {
+        rpn.push(ops.pop().op);
+      }
+      ops.push(op);
+      rpn.push(op.val);
+    });
+    while (ops.length) {
+      rpn.push(ops.pop().op)
+    }
+    let res = [];
+    rpn.forEach(token => {
+      if (typeof token === 'function') {
+        res.push(token(res.pop(), res.pop()));
+      } else {
+        res.push(token);
+      }
+    });
+    return res.pop();
   }
-  
-  subtract(number) {
-    // your implementation
+  add(val) {
+    this.operations.push({
+      op: (a, b) => a + b,
+      priority: 2,
+      val: val
+    });
+    return this;
   }
-
-  multiply(number) {
-    // your implementation
+  substract(val) {
+    this.operations.push({
+      op: (a, b) => b - a,
+      priority: 2,
+      val: val
+    })
+    return this;
   }
-
-  devide(number) {
-    // your implementation
+  multiply(val) {
+    this.operations.push({
+      op: (a, b) => a * b,
+      priority: 1,
+      val: val
+    })
+    return this;
   }
-
-  pow(number) {
-    // your implementation
+  divide(val) {
+    this.operations.push({
+      op: (a, b) => b / a,
+      priority: 1,
+      val: val
+    })
+    return this;
+  }
+  valueOf() {
+    return this.result;
   }
 }
 
